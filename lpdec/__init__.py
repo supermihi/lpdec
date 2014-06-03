005facb86e90f44482379cf5d700b6950f0266af
Michael Helmling <michaelhelmling@posteo.de>
2014-06-03 14:23:35 +0200

    Improve version checking

--- lpdec/__init__.py
+++ lpdec/__init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-# cython: embedsignature=True
 # Copyright 2014 Michael Helmling
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
 # published by the Free Software Foundation
-from __future__ import division
+from __future__ import division, unicode_literals
 
 __version__ = '2014.2'
 _exactVersion = None
@@ -38,6 +37,9 @@ def exactVersion():
     topdir = normpath(join(dirname(__file__), '..'))
     version = __version__
     if exists(join(topdir, '.git')):
-        version = check_output(['git', 'describe', '--dirty'], cwd=topdir).decode().strip()
+        try:
+            version = check_output(['git', 'describe', '--dirty'], cwd=topdir).decode().strip()
+        except OSError: # git might not be installed
+            pass
     _exactVersion = version
     return version
